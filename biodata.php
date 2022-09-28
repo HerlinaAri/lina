6b5c8f1a580faa17075221bcabccb70bb264c90b
HerlinaAri <114554341+HerlinaAri@users.noreply.github.com>
2022-09-28 11:24:07 +0700

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> Biodata</title>
+</head>
+
+<body>
+    <table border="3" style=background-color:white>
+        <tr>
+            <td style=background-color:purple colspan="3">
+                <center><b>
+                        <font color=white size=30>BIODATA</font>
+                    </b></center>
+            </td>
+        </tr>
+        <tr>
+            <td style=background-color:purple colspan="3">
+                <center><b>
+                        <font color=white size=30> Universitas Muhammadiyah Purworejo
+                        </font>
+                    </b></center>
+            </td>
+        </tr>
+        <tr>
+            <td rowspan=25>
+                <center><img src="fotoku.jpg" width=200 high=300 /></center>
+            </td>
+        </tr>
+        <tr>
+            <td>Nim</td>
+            <td>202520033</td>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td>Herlina Ari Wulanjati</td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td>kalimeneng kemiri </td>
+        </tr>
+        <tr>
+            <td>Alamat email</td>
+            <td>herlinaary18@gmail.com</td>
+        </tr>
+        <tr>
+            <td>Bidang minat</td>
+            <td>menyanyi</td>
+        </tr>
+
+    </table>
+</body>
+
+</html>
